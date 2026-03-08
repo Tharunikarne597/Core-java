8322f5773fcf5e2439100d47b153f43aaaa290f9
Tharunikarne597 <karnetharuni31@gmail.com>
2026-03-08 21:15:12 +0530

    switch.java

---
+++ switch.java
@@ -0,0 +1,21 @@
+public class  conditionalstatementspart2{
+  public static void main(String[] args){
+    int i = 5;
+    switch(i) { 
+    case 1:
+      System.out.println("this is the first statement");
+      break;
+      case 2:
+      System.out.println("this is the second statement");
+       break;
+      case 3:
+       System.out.println("this is the third statement");
+        break;
+      case 4:
+        System.out.println("this is the fourth statement");
+        break;
+      default:
+        System.out.println(" the input is inavalid");
+    }
+  }
+        
