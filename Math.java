6d6cbaebe4a69323c9accaf7cc0501fd282e72d5
Tharunikarne597 <karnetharuni31@gmail.com>
2026-03-09 11:56:16 +0530

    Math.java

---
+++ Math.java
@@ -0,0 +1,10 @@
+//the Java Math class has many methods that allows you to perform mathematical tasks on numbers.
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Math.max(5, 10));  
+    System.out.println(Math.min(5, 10));
+    System.out.println(Math.sqrt(64));
+    System.out.println(Math.abs(-4.7));  
+    System.out.println(Math.pow(2, 8));
+  }
+}
